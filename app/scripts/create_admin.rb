ca3f092d3941f7bfd11abc027237a1ae3d2ef580
Vitaly Kuleshov <kuleshovva24@gmail.com>
2026-05-30 16:40:20 +0300

    Delete unnessesary and duplicated files and turned back dicker-compose.yml

--- app/scripts/create_admin.rb
+++
@@ -1,3 +0,0 @@
-# list = ExtratingsList.find_by(name_ru: 'Top 500')
-# list.extratings_list_units.pluck(:extratings_unit_id).uniq.size # Кол-во уникальных параметров
-# list.extratings_list_units.size
